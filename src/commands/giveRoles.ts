176510b028ce9e714e592ad579ef38a68ee84dd2
svalencia014 <sv.samuel.valencia014@gmail.com>
2025-08-09 22:52:56 -0400

    continue working

--- src/commands/giveRoles.ts
+++ src/commands/giveRoles.ts
@@ -1,5 +1,5 @@
 import { ChatInputCommandInteraction, Interaction, Role, SlashCommandBuilder } from "discord.js";
-import * as config from "../config";
+import { config } from "../config";
 
 export const data = new SlashCommandBuilder()
   .setName("giveroles")
@@ -14,9 +14,26 @@ export async function execute(interaction: ChatInputCommandInteraction) {
   
   const uid = interaction.user.id;
 
-  let req = await fetch(`https://api.vatsim.net/v2/members/discord/${uid}`)
-  let res = await req.json();
-  console.log(res);
+   let cid: string;
+
+  try {
+    let req = await fetch(`https://api.vatsim.net/v2/members/discord/${uid}`)
+    let res: { id: string, user_id: string } = await req.json();
+    cid = res.user_id;
+  } catch (error) {
+    await interaction.editReply("You are not linked to a VATSIM account! Please link your account at https://community.vatsim.net/ then try again!")
+    return;
+  }
+
+  let req = await fetch(`https://api.vatusa.net/v2/user/${cid}`);
+  if (req.status == 404) {
+    // One of many ways to detect a pilot
+    let member = await interaction.guild?.members.fetch(uid);
+    member.roles.add(config.pilot);
+    await interaction.editReply("Your roles have been updated!");
+    return;
+  }
+
 
   await interaction.editReply("Your roles have been updated!");
 }
