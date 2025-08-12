fb4ac27fa42f1fc0695b235a4147ab1e06cf739b
svalencia014 <sv.samuel.valencia014@gmail.com>
2025-08-12 12:05:21 -0400

    port code

--- src/commands/giveRoles.ts
+++ src/commands/giveRoles.ts
@@ -1,4 +1,5 @@
 import { ChatInputCommandInteraction, Interaction, Role, SlashCommandBuilder } from "discord.js";
+import type { User } from 'vatusa-api-types';
 import { config } from "../config";
 
 export const data = new SlashCommandBuilder()
@@ -25,15 +26,77 @@ export async function execute(interaction: ChatInputCommandInteraction) {
     return;
   }
 
+  let member = await interaction.guild?.members.fetch(uid);
+
   let req = await fetch(`https://api.vatusa.net/v2/user/${cid}`);
   if (req.status == 404) {
     // One of many ways to detect a pilot
-    let member = await interaction.guild?.members.fetch(uid);
     member.roles.add(config.pilot);
     await interaction.editReply("Your roles have been updated!");
     return;
   }
 
+  let res = await req.json() as User;
+  console.log(res);
+
+  member.roles.add(config.controller);
+
+  switch (res.data.rating) { 
+    case 1: {
+      member.roles.add(config.OBS);
+      break;
+    }
+    case 2: {
+      member.roles.add(config.S1);
+      break;
+    }
+    case 3: {
+      member.roles.add(config.S2);
+      break;
+    }
+    case 4: {
+      member.roles.add(config.S3);
+      break;
+    }
+    case 5: {
+      member.roles.add(config.C1);
+      break;
+    }
+    //6 = C2, not used for VATSIM
+    case 7: {
+      member.roles.add(config.C3);
+      break;
+    }
+    case 8: {
+      member.roles.add(config.I1);
+      break;
+    }
+    //9 = I2, not used for VATSIM
+    case 10: {
+      member.roles.add(config.I3);
+      break;
+    }
+    case 11: {
+      member.roles.add(config.SUP);
+      break;
+    }
+    case 12: {
+      member.roles.add(config.ADM);
+      break;  
+    }
+  }
+
+  await member.setNickname(`${res.data.fname} ${res.data.lname} | ${res.data.facility}`)
+  if (res.data.facility == "ZJX") {
+    member.roles.add(config.home);
+  } else {
+    for (let visitingFacility of res.data.visiting_facilities) {
+      if (visitingFacility.facility == "ZJX") {
+        member.roles.add(config.visitor);
+        break;
+      }
+    }
+  }
 
   await interaction.editReply("Your roles have been updated!");
 }
